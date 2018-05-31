dc5e1a7f048bddc027c3b09a848a7e4dbb38e172
Wang Xin <wangx201401@126.com>
2018-05-31 21:08:14 +0800

    add volumetpye, volumesMaxSize and volumeMinSize parameters in qingStorageClass

--- pkg/block/storage_class_test.go
+++ pkg/block/storage_class_test.go
@@ -1,29 +1,68 @@
 package block
 
 import (
-	"testing"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"testing"
 )
 
-func Test_getConfigFromQingStorageClass(t *testing.T){
-	// get storage class
+func Test_getConfigFromQingStorageClass(t *testing.T) {
+	// new default storageclass
+	sc := NewDefaultQingStorageClass()
+	// get storageclass
 	var winfilepath = "C:\\Users\\wangx\\Documents\\config.json"
 	content, err := ioutil.ReadFile(winfilepath)
-	if err != nil{
+	if err != nil {
 		t.Error("Open file error: ", err.Error())
 	}
-	sc := qingStorageClass{}
 	err = json.Unmarshal(content, &sc)
-	if err != nil{
+	if err != nil {
 		t.Error("get storage class error: ", err.Error())
 	}
 
 	// print storage class
-	bytes, _:=json.Marshal(sc)
+	bytes, _ := json.Marshal(sc)
 	t.Log("storage class:", string(bytes[:]))
 	// get config
-	config := getConfigFromStorageClass(&sc)
+	config := sc.getConfig()
 	// print config
 	t.Log("config:", config)
+}
+
+func TestFormatVolumeSize(t *testing.T) {
+	// new default storageclass
+	sc := NewDefaultQingStorageClass()
+	// get storageclass
+	var winfilepath = "C:\\Users\\wangx\\Documents\\config.json"
+	content, err := ioutil.ReadFile(winfilepath)
+	if err != nil {
+		t.Error("Open file error: ", err.Error())
+	}
+	err = json.Unmarshal(content, &sc)
+	if err != nil {
+		t.Error("get storage class error: ", err.Error())
+	}
+	// testcase
+	testcase := []struct {
+		size   int
+		result int
+	}{
+		{-1, sc.VolumeMinSize},
+		{0, sc.VolumeMinSize},
+		{10, sc.VolumeMinSize},
+		{9, sc.VolumeMinSize},
+		{34, 40},
+		{258, 260},
+		{1091, sc.VolumeMaxSize},
+	}
+	for _, v := range testcase {
+		actual := sc.formatVolumeSize(v.size)
+		if actual == v.result {
+			t.Logf("testcase success")
+		} else {
+			t.Errorf("testcase size: %d, expect %d, actually %d",
+				v.size, v.result, actual)
+		}
+	}
+
 }
--- pkg/block/volume_manager_test.go
+++ pkg/block/volume_manager_test.go
@@ -1,117 +1,117 @@
 package block
 
-import(
-	"testing"
-	"io/ioutil"
+import (
 	"encoding/json"
-	"os"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strconv"
+	"testing"
 )
 
-var getvp = func() *volumeProvisioner{
+var getvp = func() *volumeProvisioner {
 	// get storage class
 	var winfilepath = "C:\\Users\\wangx\\Documents\\config.json"
 	content, err := ioutil.ReadFile(winfilepath)
-	if err != nil{
+	if err != nil {
 		fmt.Errorf("Open file error: %s", err.Error())
 		os.Exit(-1)
 	}
 	sc := qingStorageClass{}
 	err = json.Unmarshal(content, &sc)
-	if err != nil{
+	if err != nil {
 		fmt.Errorf("get storage class error: %s", err.Error())
 		os.Exit(-1)
 	}
 
 	// get volume provisioner
 	vp, err := newVolumeProvisioner(&sc)
-	if err != nil{
+	if err != nil {
 		fmt.Errorf("new volume provisioner error: %s", err.Error())
 		os.Exit(-1)
 	}
 	return vp
 }
 
-func TestFindVolume(t *testing.T){
+func TestFindVolume(t *testing.T) {
 	// testcase
-	testcase := []struct{
-		id string
+	testcase := []struct {
+		id    string
 		exist bool
 	}{
 		{"vol-fhlkhxpr", true},
-		{"vol-vol-fhlkhxpw",false},
+		{"vol-vol-fhlkhxpw", false},
 	}
 
 	vp := getvp()
 	// test findVolume
-	for _, v:= range testcase{
+	for _, v := range testcase {
 		flag, err := vp.findVolume(v.id)
-		if err != nil{
+		if err != nil {
 			t.Error("find volume error: ", err.Error())
 		}
-		if (flag != nil) == v.exist{
+		if (flag != nil) == v.exist {
 			t.Logf("volume id %s, expect %t, actually %t", v.id, v.exist, flag != nil)
-		}else{
+		} else {
 			t.Errorf("volume id %s, expect %t, actually %t", v.id, v.exist, flag != nil)
 		}
 	}
 }
 
-func TestCreateVolume(t *testing.T){
+func TestCreateVolume(t *testing.T) {
 	// testcase
-	testcase := []struct{
-		vc volumeClaim
+	testcase := []struct {
+		vc            volumeClaim
 		createSuccess bool
 	}{
-		{volumeClaim{VolName:"pvc-test-", VolType:"hp", VolSizeRequest:12},true},
-		{volumeClaim{VolName:"pvc-test-", VolType:"hp", VolSizeRequest:121},true},
-		{volumeClaim{VolName:"pvc-test-", VolType:"hp", VolSizeRequest:-1},false},
+		{volumeClaim{VolName: "pvc-test-", VolSizeRequest: 12}, true},
+		{volumeClaim{VolName: "pvc-test-", VolSizeRequest: 121}, true},
+		{volumeClaim{VolName: "pvc-test-", VolSizeRequest: -1}, true},
 	}
-	vp:=getvp()
-	for i,v:=range testcase{
+	vp := getvp()
+	for i, v := range testcase {
 		v.vc.VolName += strconv.Itoa(i)
 		err := vp.CreateVolume(&v.vc)
-		if (err == nil)== v.createSuccess{
+		if (err == nil) == v.createSuccess {
 			t.Logf("testcase passed, %v", v)
-		}else{
+		} else {
 			t.Error(err)
 		}
 	}
 }
 
-func TestDeleteVolume(t *testing.T){
-	vp:=getvp()
-	volumeID := "vol-az6ofhp5"
-	err :=vp.DeleteVolume(volumeID)
-	if err != nil{
+func TestDeleteVolume(t *testing.T) {
+	vp := getvp()
+	volumeID := "vol-vhvb1il0"
+	err := vp.DeleteVolume(volumeID)
+	if err != nil {
 		t.Error(err)
-	}else{
+	} else {
 		t.Logf("testcase delete %s success", volumeID)
 	}
 }
 
-func TestAttachVolume(t *testing.T){
-	vp:=getvp()
+func TestAttachVolume(t *testing.T) {
+	vp := getvp()
 	volumeID := "vol-fhlkhxpr"
-	instanceID:= "i-msu2th7i"
+	instanceID := "i-msu2th7i"
 	err := vp.AttachVolume(&volumeID, &instanceID)
-	if err != nil{
+	if err != nil {
 		t.Error(err)
-	}else{
+	} else {
 		t.Logf("testcase attach volume %s to instance %s success",
 			volumeID, instanceID)
 	}
 }
 
-func TestDetachVolume(t *testing.T){
-	vp:=getvp()
+func TestDetachVolume(t *testing.T) {
+	vp := getvp()
 	volumeID := "vol-fhlkhxpr"
-	instanceID:= "i-msu2th7i"
+	instanceID := "i-msu2th7i"
 	err := vp.DetachVolume(&volumeID, &instanceID)
-	if err != nil{
+	if err != nil {
 		t.Error(err)
-	}else{
+	} else {
 		t.Logf("testcase detach volume %s from instance %s success",
 			volumeID, instanceID)
 	}
