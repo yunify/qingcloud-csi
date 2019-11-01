11f46f952805de2771de572939e57d0f8f20e885
Xin Wang <wileywang@yunify.com>
2019-11-01 18:40:47 +0800

    Add driver util ut

--- pkg/disk/driver/utils.go
+++ pkg/disk/driver/utils.go
@@ -17,7 +17,6 @@ limitations under the License.
 package driver
 
 import (
-	"fmt"
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"github.com/yunify/qingcloud-csi/pkg/common"
 	"io/ioutil"
@@ -53,29 +52,8 @@ func IsValidFileSystemType(fs string) bool {
 	}
 }
 
-// FormatVolumeSize transfer to proper volume size
-func FormatVolumeSize(volType VolumeType, volSize int) int {
-	_, ok := VolumeTypeName[volType]
-	if ok == false {
-		return -1
-	}
-	volTypeMinSize := VolumeTypeToMinSize[volType]
-	volTypeMaxSize := VolumeTypeToMaxSize[volType]
-	volTypeStepSize := VolumeTypeToStepSize[volType]
-	if volSize <= volTypeMinSize {
-		return volTypeMinSize
-	} else if volSize >= volTypeMaxSize {
-		return volTypeMaxSize
-	}
-	if volSize%volTypeStepSize != 0 {
-		volSize = (volSize/volTypeStepSize + 1) * volTypeStepSize
-	}
-	if volSize >= volTypeMaxSize {
-		return volTypeMaxSize
-	}
-	return volSize
-}
-
+// GetInstanceIdFromFile gets instance id from specific file path.
+// In QingCloud Linux instance, it stores instance id in /etc/qingcloud/instance-id.
 func GetInstanceIdFromFile(filepath string) (instanceId string, err error) {
 	bytes, err := ioutil.ReadFile(filepath)
 	if err != nil {
@@ -87,10 +65,12 @@ func GetInstanceIdFromFile(filepath string) (instanceId string, err error) {
 	return instanceId, nil
 }
 
+// NewVolumeCapabilityAccessMode creates CSI volume access mode object.
 func NewVolumeCapabilityAccessMode(mode csi.VolumeCapability_AccessMode_Mode) *csi.VolumeCapability_AccessMode {
 	return &csi.VolumeCapability_AccessMode{Mode: mode}
 }
 
+// NewControllerServiceCapability creates CSI controller capability object.
 func NewControllerServiceCapability(cap csi.ControllerServiceCapability_RPC_Type) *csi.ControllerServiceCapability {
 	return &csi.ControllerServiceCapability{
 		Type: &csi.ControllerServiceCapability_Rpc{
@@ -101,6 +81,7 @@ func NewControllerServiceCapability(cap csi.ControllerServiceCapability_RPC_Type
 	}
 }
 
+// NewNodeServiceCapability creates CSI node capability object.
 func NewNodeServiceCapability(cap csi.NodeServiceCapability_RPC_Type) *csi.NodeServiceCapability {
 	return &csi.NodeServiceCapability{
 		Type: &csi.NodeServiceCapability_Rpc{
@@ -109,63 +90,4 @@ func NewNodeServiceCapability(cap csi.NodeServiceCapability_RPC_Type) *csi.NodeS
 			},
 		},
 	}
-}
-
-func validateVolumeCapabilities(vcs []*csi.VolumeCapability) error {
-	isMnt := false
-	isBlk := false
-
-	if vcs == nil {
-		return fmt.Errorf("volume capabilities is nil")
-	}
-
-	for _, vc := range vcs {
-		if err := validateVolumeCapability(vc); err != nil {
-			return err
-		}
-		if blk := vc.GetBlock(); blk != nil {
-			isBlk = true
-		}
-		if mnt := vc.GetMount(); mnt != nil {
-			isMnt = true
-		}
-	}
-
-	if isBlk && isMnt {
-		return fmt.Errorf("both mount and block volume capabilities specified")
-	}
-
-	return nil
-}
-
-func validateVolumeCapability(vc *csi.VolumeCapability) error {
-	if err := validateAccessMode(vc.GetAccessMode()); err != nil {
-		return err
-	}
-	blk := vc.GetBlock()
-	mnt := vc.GetMount()
-	if mnt == nil && blk == nil {
-		return fmt.Errorf("must specify an access type")
-	}
-	if mnt != nil && blk != nil {
-		return fmt.Errorf("specified both mount and block access types")
-	}
-	return nil
-}
-
-func validateAccessMode(am *csi.VolumeCapability_AccessMode) error {
-	if am == nil {
-		return fmt.Errorf("access mode is nil")
-	}
-
-	switch am.GetMode() {
-	case csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER:
-	case csi.VolumeCapability_AccessMode_SINGLE_NODE_READER_ONLY:
-	case csi.VolumeCapability_AccessMode_MULTI_NODE_READER_ONLY:
-	case csi.VolumeCapability_AccessMode_MULTI_NODE_MULTI_WRITER:
-		return fmt.Errorf("MULTI_NODE_MULTI_WRITER access mode is not yet supported for PD")
-	default:
-		return fmt.Errorf("%v access mode is not supported for for PD", am.GetMode())
-	}
-	return nil
 }
